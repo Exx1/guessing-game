50df59a70c95b2e2d9844cd26a913d2860f18622
Exx1 <exx-3@yandex.ru>
2022-08-10 14:39:09 +0300

    init

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const GuessingGame = require('./src/guessing-game.js');
 
 window.game = new GuessingGame();
+
+class GuessingGame {
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+    
+    guess()
+}
+
+console.log(min);
