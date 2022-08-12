ddbbee1588b2c489dc6f98b80233cb38d4b4ac40
Exx1 <exx-3@yandex.ru>
2022-08-12 10:27:49 +0300

    feat: half complited

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,39 @@
+let option;
+    let arr;
+    let guessResult;
+
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
-
+    
+    
+        setRange(min, max) {
+            arr = Array.from(Array(max).keys());
+            option = (arr.length);
+        }
+        
+        guess() {
+            guessResult = arr[Math.ceil(arr.length / 2)];
+            option = Math.ceil(option / 2);
+            console.log('guessResult: ' + guessResult);
+            return guessResult;
+        }
+    
+        lower() {
+            console.log('lower');
+            console.log(option);
+            arr = arr.slice(0, option);
+            console.log(arr);
+        }
+    
+        greater() {
+            console.log('greater');
+            console.log(option);
+            arr = arr.slice(option);
+            console.log(arr);
+        }
+    
     }
 
-    guess() {
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
-
-module.exports = GuessingGame;
+    module.exports = GuessingGame;
